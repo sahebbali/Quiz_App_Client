00ecf0f20497644b56afc6dd08ffbbd65ea3fab6
Saheb Ali <you@example.com>
2022-12-02 22:53:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import  './styles/App.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+/** import components */
+import Main from './components/Home';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import { CheckUserExist } from './helper/helper';
+
+/** react routes */
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Main></Main>
+  },
+  {
+    path : '/quiz',
+    element : <CheckUserExist><Quiz/> </CheckUserExist> 
+  },
+  { 
+    path : '/result',
+    element : <Result></Result>
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
